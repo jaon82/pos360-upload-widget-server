3fb3a447b244ae3b78024536b9403c5cc6f055a4
Jean Nunes <jean.nunes@uzzipay.com>
2025-02-08 16:12:36 -0400

    Set HTTP server with Fastify

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,8 +1,36 @@
 import fastifyCors from '@fastify/cors';
 import fastify from 'fastify';
+import {
+  hasZodFastifySchemaValidationErrors,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod';
+import { uploadImageRoute } from './routes/upload-image';
 
 const server = fastify();
+
+server.setValidatorCompiler(validatorCompiler);
+server.setSerializerCompiler(serializerCompiler);
+
+server.setErrorHandler((error, request, reply) => {
+  if (hasZodFastifySchemaValidationErrors(error)) {
+  }
+  reply.status(400).send({
+    message: 'Validation error.',
+    issues: error.validation,
+  });
+
+  //envia o erro para ferramenta de observabilidade (Sentry/Datadog/Grafana/OTel)
+  console.log(error);
+  return reply.status(500).send({
+    message: 'Internal server error.',
+  });
+});
+
 server.register(fastifyCors, { origin: '*' });
+
+server.register(uploadImageRoute);
+
 server.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
   console.log('HTTP Server running...');
 });
