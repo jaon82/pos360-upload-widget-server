8b3cb67b8664b804d0d2923eb4437c9fc5528dad
Jean Nunes <jean.nunes@uzzipay.com>
2025-05-07 00:44:44 -0400

    Configure App Runner deploy

---
+++ src/infra/http/routes/health-check.ts
@@ -0,0 +1,7 @@
+import type { FastifyInstance } from 'fastify';
+
+export const healthCheckRoute = async (server: FastifyInstance) => {
+  server.get('/health', async (request, reply) => {
+    return reply.status(200).send({ message: 'OK' });
+  });
+};
