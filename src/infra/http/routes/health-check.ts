877b654aa05e07661d4868f44860beaae8fb54aa
Jean Nunes <jean.nunes@uzzipay.com>
2025-05-10 11:23:43 -0400

    Configure ECS deploy vars

--- src/infra/http/routes/health-check.ts
+++ src/infra/http/routes/health-check.ts
@@ -2,6 +2,6 @@ import type { FastifyInstance } from 'fastify';
 
 export const healthCheckRoute = async (server: FastifyInstance) => {
   server.get('/health', async (request, reply) => {
-    return reply.status(200).send({ message: 'ECS Health Check OK' });
+    return reply.status(200).send({ message: 'Health Check OK' });
   });
 };
