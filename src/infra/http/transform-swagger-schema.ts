b4e93591aa2a228dcfc80c5753a53548c9002cfd
Jean Nunes <jaon82@gmail.com>
2026-04-15 12:48:35 -0300

    refactor(swagger): improve robustness of schema transformation for consumes

--- src/infra/http/transform-swagger-schema.ts
+++ src/infra/http/transform-swagger-schema.ts
@@ -5,7 +5,7 @@ type TransformSwaggerSchemaDAta = Parameters<typeof jsonSchemaTransform>[0];
 export function transformSwaggerSchema(data: TransformSwaggerSchemaDAta) {
   const { schema, url } = jsonSchemaTransform(data);
 
-  if (schema.consumes?.includes('multipart/form-data')) {
+  if (schema?.consumes?.includes('multipart/form-data')) {
     if (schema.body === undefined) {
       schema.body = {
         type: 'object',
